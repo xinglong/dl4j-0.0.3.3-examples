eaa8ec8758fd16cbea21346d8cf22fcb5996c33a
agibsonccc <0@blix.io>
2015-01-06 19:23:11 -0800

    add DR example

--- src/main/java/org/deeplearning4j/mnist/dimensionalityreduction/DimensionalityReduction.java
+++ src/main/java/org/deeplearning4j/mnist/dimensionalityreduction/DimensionalityReduction.java
@@ -1,4 +1,4 @@
-package org.deeplearning4j.mnist.small;
+package org.deeplearning4j.mnist.dimensionalityreduction;
 
 import org.apache.commons.math3.random.MersenneTwister;
 import org.apache.commons.math3.random.RandomGenerator;
@@ -10,11 +10,9 @@ import org.deeplearning4j.models.featuredetectors.rbm.RBM;
 import org.deeplearning4j.nn.api.LayerFactory;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.layers.OutputLayer;
 import org.deeplearning4j.nn.layers.factory.LayerFactories;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
-import org.nd4j.linalg.api.activation.Activations;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
@@ -25,9 +23,9 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by agibsonccc on 9/11/14.
  */
-public class DBNExample {
+public class DimensionalityReduction {
 
-    private static Logger log = LoggerFactory.getLogger(DBNExample.class);
+    private static Logger log = LoggerFactory.getLogger(DimensionalityReduction.class);
 
 
     public static void main(String[] args) throws Exception {
@@ -37,20 +35,8 @@ public class DBNExample {
                 .iterations(5).layerFactory(l)
                 .lossFunction(LossFunctions.LossFunction.RECONSTRUCTION_CROSSENTROPY).rng(gen)
                 .learningRate(1e-1f).nIn(784).nOut(10).list(4)
-                .hiddenLayerSizes(new int[]{600, 500, 400}).override(new NeuralNetConfiguration.ConfOverride() {
-                    @Override
-                    public void override(int i, NeuralNetConfiguration.Builder builder) {
-                        if (i == 3) {
-                            builder.layerFactory(LayerFactories.getFactory(OutputLayer.class));
-                            builder.weightInit(WeightInit.ZERO);
-                            builder.activationFunction(Activations.softMaxRows());
-                            builder.lossFunction(LossFunctions.LossFunction.MCXENT);
-
-                        }
-                    }
-                })
+                .hiddenLayerSizes(new int[]{600, 500, 400})
                 .build();
-
 
 
 
@@ -60,22 +46,12 @@ public class DBNExample {
 
         DataSetIterator iter = new MultipleEpochsIterator(10,new MnistDataSetIterator(1000,1000));
         network.fit(iter);
+        iter.reset();
+        //dimensionality reduced matrix
+        INDArray output = network.output(iter.next().getFeatureMatrix());
 
 
         iter.reset();
-
-        Evaluation eval = new Evaluation();
-
-        while(iter.hasNext()) {
-
-            DataSet d2 = iter.next();
-            INDArray predict2 = network.output(d2.getFeatureMatrix());
-
-            eval.eval(d2.getLabels(), predict2);
-
-        }
-
-        log.info(eval.stats());
 
 
     }
