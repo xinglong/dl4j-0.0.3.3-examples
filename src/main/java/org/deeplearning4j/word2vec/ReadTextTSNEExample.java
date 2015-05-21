3cf0bc276bc5425d95ee10791ccc5f1ed3848639
nyghtowl <warrick.melanie@gmail.com>
2015-05-21 14:34:05 -0700

    Revised and standardized examples. Also consolidated tsne examples with word2vec.

--- src/main/java/org/deeplearning4j/word2vec/ReadTextTSNEExample.java
+++ src/main/java/org/deeplearning4j/word2vec/ReadTextTSNEExample.java
@@ -11,9 +11,9 @@ import java.io.File;
 /**
  * Created by agibsonccc on 10/9/14.
  */
-public class ReadTextTSNEExample {
+public class ReadTextTsneExample {
 
-    private static Logger log = LoggerFactory.getLogger(ReadTextTSNEExample.class);
+    private static Logger log = LoggerFactory.getLogger(ReadTextTsneExample.class);
 
     public static void main(String[] args) throws Exception {
 
